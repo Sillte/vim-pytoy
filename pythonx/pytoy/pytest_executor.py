9a56af0f9c451f450102dda5f66b7f1c883d40cf
sillte <innocent.konoha@gmail.com>
2025-05-24 11:15:00 +0900

    Add MypyExecutor

--- pythonx/pytoy/pytest_executor.py
+++ pythonx/pytoy/pytest_executor.py
@@ -1,14 +1,10 @@
-import re
-from typing import Dict, List
-
 import vim
+import re 
 
 from pytoy.executors import BufferExecutor
 
 from pytoy.ui_utils import (
-    to_buffer_number,
     init_buffer,
-    create_window,
     store_window,
     to_buffer,
 )
@@ -34,7 +30,7 @@ class PytestExecutor(BufferExecutor):
         """Execute `naive`, `pytest`."""
         command = "pytest"
         if self.with_uv:
-            commant = f"uv run {command}"
+            command = f"uv run {command}"
         init_buffer(stdout)
         return super().run(command, stdout)
 
@@ -42,7 +38,7 @@ class PytestExecutor(BufferExecutor):
         """Execute `pytest` for only one file."""
         command = f'pytest "{path}"'
         if self.with_uv:
-            commant = f"uv run {command}"
+            command = f"uv run {command}"
         init_buffer(stdout)
         return super().run(command, stdout)
 
@@ -91,4 +87,65 @@ class PytestExecutor(BufferExecutor):
 
     def _make_qflist(self, string):
         records = PytestDecipher(string).records
+        return records
+
+
+class MypyExecutor(BufferExecutor):
+    def __new__(cls, name=None, with_uv=None):
+        if with_uv is None:
+            e_mode = get_default_execution_mode()
+            if e_mode == ExecutionMode.WITH_UV:
+                with_uv = True
+            else:
+                with_uv = False
+        self = super().__new__(cls, name)
+        self.with_uv = with_uv
+        self._pattern = re.compile(r"(?P<filename>.+):(?P<lnum>\d+):(?P<col>\d+):(?P<_type>(.+)):(?P<text>(.+))")
+        return self
+
+    def runfile(self, path, stdout):
+        """Execute `pytest` for only one file."""
+        command = f'mypy --show-traceback --show-column-numbers "{path}"'
+        if self.with_uv:
+            command = f"uv run {command}"
+        stdout = to_buffer(stdout)
+        init_buffer(stdout)
+        #stdout[0] = command
+        return super().run(command, stdout)
+
+    def prepare(self):
+        # I do not need to return any `dict` for customization.
+        # But, it requires `win_id` at `on_closed`.
+        self.win_id = vim.eval("win_getid()")
+        return None
+
+    def on_closed(self):
+        # vim.Function("setloclist") seems to more appropriate,
+        # but it does not work correctly with Python 3.9?
+        setloclist = vim.bindeval('function("setloclist")')
+        messages = "\n".join(line for line in self.stdout)
+        qflist = self._make_qflist(messages)
+        if qflist:
+            setloclist(self.win_id, qflist)
+        else:
+            with store_window():
+                vim.eval(f"win_gotoid({self.win_id})")
+                vim.command(f"lclose")
+
+        # Scrolling output window
+        with store_window():
+            stdout_id = vim.eval(f"bufwinid({self.stdout.number})")
+            vim.command(f"call win_gotoid({stdout_id})")
+            vim.command(f"normal zb")
+
+    def _make_qflist(self, string):
+        # Record of `mypy`. 
+        records = []
+        for line in string.split("\n"):
+            match = self._pattern.match(line.strip())
+            if not match:
+                continue
+            record = match.groupdict()
+            record["type"] = record["_type"].strip(" ")[0].upper()
+            records.append(record)
         return records
