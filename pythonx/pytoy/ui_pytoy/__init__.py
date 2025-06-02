14c9b55dba34443b528a54bb6021d09382dd279a
sillte <innocent.konoha@gmail.com>
2025-06-03 00:09:18 +0900

    ui_pytoy.buffer is introduced

---
+++ pythonx/pytoy/ui_pytoy/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+This module is intended to provide the common interface of ui.
+
+* vim
+* neovim
+* neovim+vscode
+
+Usage: executors:
+
+"""
+
+from pytoy.ui_pytoy.ui_enum import UIEnum, get_ui_enum #  NOQA
+from pytoy.ui_pytoy.pytoy_buffer import PytoyBuffer, make_buffer, make_duo_buffers  # NOQA
+
