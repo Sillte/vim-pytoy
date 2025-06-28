d5b6ed4b1bccfa17fa9f3339ec35bb2fbb6da176
sillte <innocent.konoha@gmail.com>
2025-06-28 23:30:14 +0900

    Refactor

---
+++ pythonx/pytoy/infra/command/__init__.py
@@ -0,0 +1,341 @@
+import shlex
+from typing import Callable
+from typing import TypeAlias
+import inspect
+
+import vim
+
+from pytoy.infra.command.range_count_option import RangeCountOption, RangeCountType
+from pytoy.infra.command._opts_converter import _OptsConverter
+from pytoy.infra.command._customlist_manager import _CustomListManager
+
+from pytoy.infra.command._opts_converter import OptsArgument  # NOQA
+
+
+class CommandManager:
+    FUNCTION_MAPS = dict()
+    COMMAND_MAPS = dict()
+    CONVERTER_MAPS = dict()
+
+    @classmethod
+    def register(
+        cls,
+        name: str,
+        nargs=None,
+        range=None,
+        count=None,
+        complete=None,
+        addr=None,
+        *,
+        exist_ok: bool = False,
+    ):
+        """Register the commands
+        """
+        rc_opt = RangeCountOption(range, count)
+
+        def _inner(target):
+            nonlocal complete
+
+            def _is_function_target(target):
+                return inspect.isfunction(target) or isinstance(target, staticmethod)
+
+            if isinstance(target, classmethod):
+                raise ValueError(
+                    f"Classmethod cannot be registered, {target=}, {name=}"
+                )
+
+            if _is_function_target(target):
+                cls._handle_existent_command(name=name, exist_ok=exist_ok)
+                if _is_function_target(complete):
+                    c_vimfunc_name = _CustomListManager.register(name, complete)
+                    complete = f"customlist,{c_vimfunc_name}"
+
+                return cls._register_for_func(
+                    target,
+                    name,
+                    nargs,
+                    range_count_option=rc_opt,
+                    complete=complete,
+                    addr=addr,
+                )
+            elif inspect.isclass(target):
+                instance = target()
+                assert callable(instance), "CommandClass must implement `__call__`"
+
+                # [NOTE]: I am wondering whether we should be whether we should be able
+                # to get `name` from `instance property`.
+                cls._handle_existent_command(name=name, exist_ok=exist_ok)
+
+                if complete is None and hasattr(instance, "customlist"):
+                    c_vimfunc_name = _CustomListManager.register(
+                        name, getattr(instance, "customlist")
+                    )
+                    complete = f"customlist,{c_vimfunc_name}"
+
+                return cls._register_for_func(
+                    instance,
+                    name,
+                    nargs,
+                    range_count_option=rc_opt,
+                    complete=complete,
+                    addr=addr,
+                )
+            else:
+                raise ValueError("The object of registration is illegal.")
+
+        return _inner
+
+
+    @classmethod
+    def deregister(cls, name: str, no_exist_ok: bool = True):
+        """De-register the `name` commmand.
+        """
+        if not name in cls.COMMAND_MAPS:
+            if no_exist_ok:
+                return
+            else:
+                raise ValueError(f"The `command` is NOT registered. `{name=}`")
+        _CustomListManager.deregister(name)
+        cls._deregister_for_func(name)
+
+    @classmethod
+    def is_exist(cls, name: str) -> bool:
+        """Return whether the given `name` is already registered or not."""
+        return name in cls.COMMAND_MAPS
+
+    @classmethod
+    def _register_for_func(
+        cls,
+        func: Callable,
+        name: str,
+        nargs=None,
+        range_count_option: None | RangeCountOption = None,
+        complete=None,
+        addr=None,
+    ) -> Callable:
+        # vim_funcname = f"PytoyFunc_FOR_COMMAND_{name}"
+        vim_funcname = cls.to_vimfunc_name(name)
+        if name in cls.COMMAND_MAPS:
+            raise ValueError(f"The same `command` is already registered. `{name=}`")
+        if vim_funcname in cls.FUNCTION_MAPS:
+            raise ValueError(
+                "Already `FUNCTION` is defined? (Very rare situation.), {vim_funcname}"
+            )
+
+        if range_count_option is None:
+            range_count_option = RangeCountOption()
+
+        converter = _OptsConverter(func, nargs, range_count_option)
+        # Note that `OptsConverter` verifies and infers the options.
+        # Hence, at this juncture, the command options is resolved.
+        cls.CONVERTER_MAPS[name] = converter
+        nargs = converter.nargs
+        rc_opt = converter.range_count_option
+
+        class_uri = cls.get_class_uri()
+        # `procedure` is inside the function.
+        procedure = f"""python3 << EOF
+opts = {class_uri}._iterpret_opt_argument(**{rc_opt.to_dict()})
+args, kwargs = {class_uri}.CONVERTER_MAPS['{name}'](opts)
+{class_uri}.FUNCTION_MAPS['{vim_funcname}'](*args, **kwargs)
+EOF""".strip()
+        vim.command(
+            f"""function! {vim_funcname}({cls._make_vimfunc_params(rc_opt)}) 
+            {procedure}
+            endfunction
+            """
+        )
+
+        command = cls._make_command(nargs, name, vim_funcname, rc_opt, complete, addr)
+        # print("command", command)
+        vim.command(command)
+        assert vim_funcname not in cls.FUNCTION_MAPS, "Duplicated Command"
+        cls.FUNCTION_MAPS[vim_funcname] = func
+        cls.COMMAND_MAPS[name] = command
+
+        return func
+
+    @classmethod
+    def _make_vimfunc_params(
+        cls,
+        range_count_option: RangeCountOption,
+    ) -> str:
+        """
+        Based on `range` and `count` option, determine the parameters
+        of the vim-function.
+        The candidates are as follows.
+        (line1, line2, q_args)
+        (count, q_args)
+        (q_args)
+        """
+        type_, _ = range_count_option.pair
+        if type_ == RangeCountType.RANGE:
+            return ",".join(["line1", "line2", "q_args"])
+        elif type_ == RangeCountType.COUNT:
+            return ",".join(["count", "q_args"])
+        else:
+            assert type_ is RangeCountType.NONE
+            return ",".join(["q_args"])
+
+    @classmethod
+    def _make_command(
+        cls,
+        nargs: str | int,
+        command_name: str,
+        function_name: str,
+        range_count_option: RangeCountOption,
+        complete: str | None = None,
+        addr: str | None = None,
+    ):
+        """Note that the"""
+
+        if isinstance(nargs, str):
+            assert nargs in {"*", "?", "+"}
+
+        # Current implentation deficiency
+        assert addr is None
+
+        result = f"command! -nargs={nargs} "
+
+        type_, value = range_count_option.pair
+        if type_ == RangeCountType.RANGE:
+            if value and (value is not True):
+                result += f" -range={value} "
+            else:
+                result += " -range"
+        elif type_ == RangeCountType.COUNT:
+            if value and (value is not True):
+                result += f" -count={value} "
+            else:
+                result += " -count "
+
+        if complete is not None:
+            result += f" -complete={complete} "
+        # [TODO](Other options...)
+
+        result += f" {command_name} "
+
+        result += f" call {function_name}"
+
+        parameters = []
+        if type_ == RangeCountType.RANGE:
+            parameters += ["<line1>", "<line2>"]
+        elif type_ == RangeCountType.COUNT:
+            parameters.append("<count>")
+
+        parameters.append("<q-args>")
+
+        result += f"({','.join(parameters)})"
+        return result
+
+    @classmethod
+    def _iterpret_opt_argument(cls, count=None, range=None):
+        """This function interprets when the original function is as below.
+        This function is immediately called from `VimFunctions` to revolve
+        the information about arguments.
+
+        def command_func(opts: dict):
+            pass
+
+        Note that this interface mimics as `lua`.
+
+        [TODO]: `<mods>`, `<bang>` cannot be handled yet.
+
+        # This function is very closedly connected to `_make_command`.
+        # Especially, for the number of arguments of `vim_function` .
+        """
+        opts = dict()
+
+        opts["args"] = vim.eval("a:q_args")
+        # [TODO] This is crude implementation
+        # It is desirable to approach to the "true" implementation.
+        # For example, this cannot handle the case where `space` is included
+        # inside the '"'.
+        opts["fargs"] = [elem for elem in shlex.split(opts["args"])]
+
+        if count is not None:
+            # Func(count, q_args)
+            # (<count>, <q-args>)
+            opts["count"] = int(vim.eval("a:count"))
+        elif range is not None:
+            # Func(line1, line2, q_args)
+            # (<count>, <q-args>)
+            opts["line1"] = int(vim.eval("a:line1"))
+            opts["line2"] = int(vim.eval("a:line2"))
+            opts["range"] = opts["line2"] - opts["line1"] + 1
+        return opts
+
+    @classmethod
+    def get_class_uri(cls):
+        try:
+            prefix = f"{__name__}."
+        except:
+            prefix = ""
+        return f"{prefix}CommandManager"
+
+    @classmethod
+    def to_vimfunc_name(cls, name: str) -> str:
+        vim_funcname = f"PytoyFunc_FOR_COMMAND_{name}"
+        return vim_funcname
+
+    @classmethod
+    def _deregister_for_func(cls, name: str):
+        vim_funcname = cls.to_vimfunc_name(name)
+        vim.command(f"delfunction {vim_funcname}")
+        vim.command(f"delcommand {name}")
+
+        del cls.FUNCTION_MAPS[vim_funcname]
+        del cls.COMMAND_MAPS[name]
+        del cls.CONVERTER_MAPS[name]
+
+    @classmethod
+    def _handle_existent_command(cls, name: str, exist_ok: bool):
+        if cls.is_exist(name):
+            if exist_ok:
+                cls.deregister(name=name, no_exist_ok=False)
+            else:
+                raise ValueError(f"The same `command` is already registered. `{name=}`")
+            
+
+Command: TypeAlias = CommandManager
+
+
+if __name__ == "__main__":
+    # Below are the test functions.
+    pass
+
+    @CommandManager.register(name="MockCommandNoArg")
+    def mock_command():
+        print("Hellomock")
+
+    @CommandManager.register(name="MockCommandDictArg")
+    def mock_command(opts: dict):
+        print("Hellomock", opts)
+
+    def crude(a, b, c):
+        return [str(a), str(b), str(c), "curud"]
+
+    @CommandManager.register(
+        name="MockCommandStrArg", complete=lambda a, b, c: [str(a), str(b), str(c)]
+    )
+    def mock_command(s: str):
+        print("HelloStr", s)
+
+    @CommandManager.register(name="MockClass")
+    class Mock:
+        def __call__(self, s: str = "optional"):
+            print("HelloStrACLASS", s)
+
+    @CommandManager.register(name="MockTarget")
+    class Target:
+        def __call__(self, s: str = "optional"):
+            print(s)
+
+        def customlist(self, arg_lead: str, cmd_line: str, cursor_pos: int):
+            candidates = ['"DAFA"', "'AAAC'", "'AABC'"]
+            valid_candidates = [
+                elem for elem in candidates if elem.startswith(arg_lead)
+            ]
+            if valid_candidates:
+                return valid_candidates
+            return candidates
---
+++ pythonx/pytoy/infra/timertask.py
@@ -0,0 +1,107 @@
+import vim
+
+import inspect
+class TimerTask:
+    """Using `timer_start` function, it enables asyncronous process 
+    executed in VIM main loop. 
+    Especially, it is expected to use updating UI of VIM.
+
+    """
+    FUNCTION_MAP = dict() # name -> function
+    TIMER_MAP = dict()  # name -> timer-id 
+    VIMFUNCNAME_MAP = dict() # name -> vim_funcname
+
+    counter = 0
+
+    @classmethod
+    def register(cls, func, interval:int=100, name: str | None= None) -> str:
+        """Register the function without the argument. 
+        """
+        interval = int(interval)
+        sig = inspect.signature(func)
+        if len(sig.parameters) != 0:
+            raise ValueError("Callback must be without parameters.")
+        
+        if name is None:
+            name = f"AUTONAME{cls.counter}"
+
+        vim_funcname = f"LoopTask_{name}_{id(func)}"
+        if __name__ != "__main__":
+            prefix = f"{__name__}."
+            import_prefix = f"from {__name__} import TimerTask; "
+        else:
+            prefix = "" 
+            import_prefix = f" "
+        
+        procedures = f"python3 {import_prefix} {prefix}TimerTask.FUNCTION_MAP['{name}']()"
+
+        vim.command(f"""function! {vim_funcname}(timer) 
+            {procedures}
+            endfunction
+            """)
+        timer_id = int(vim.eval(f"timer_start({interval}, '{vim_funcname}', {{'repeat': -1}})"))
+
+        cls.FUNCTION_MAP[name] = func
+        cls.VIMFUNCNAME_MAP[name] = vim_funcname
+        cls.TIMER_MAP[name] = timer_id
+        cls.counter += 1
+        return name
+
+    @classmethod
+    def deregister(cls, name: str):
+        if name not in cls.FUNCTION_MAP:
+            raise ValueError("No `{name=}` exist for deregistration")
+
+        timer_id = cls.TIMER_MAP[name]
+        vim.eval(f"timer_stop({timer_id})")
+        vim_funcname = cls.VIMFUNCNAME_MAP[name]
+        vim.command(f"delfunction! {vim_funcname}")
+
+        del cls.TIMER_MAP[name]
+        del cls.VIMFUNCNAME_MAP[name]
+        del cls.FUNCTION_MAP[name]
+
+    @classmethod
+    def is_registered(cls, name: str):
+        return name in cls.TIMER_MAP
+
+    @classmethod
+    def execute_oneshot(cls, func, interval:int=100, name: str | None= None):
+        """Execute the function only one time
+        """
+        interval = int(interval)
+        sig = inspect.signature(func)
+        if len(sig.parameters) != 0:
+            raise ValueError("Callback must be without parameters.")
+        
+        if name is None:
+            name = f"ONESHOT_AUTONAME{cls.counter}"
+        cls.counter += 1
+
+        vim_funcname = f"OneShotTask_{name}_{id(func)}"
+
+        procedures = f"""
+python3 << EOF
+from {__name__} import TimerTask
+TimerTask.FUNCTION_MAP['{name}']()
+del TimerTask.FUNCTION_MAP['{name}']
+del TimerTask.VIMFUNCNAME_MAP['{name}']
+EOF
+    """.strip()
+
+        vim.command(f"""function! {vim_funcname}(timer) 
+            {procedures}
+            call timer_start(10, {{ -> execute('delfunction! {vim_funcname}') }})
+            endfunction
+            """)
+
+        cls.FUNCTION_MAP[name] = func
+        cls.VIMFUNCNAME_MAP[name] = vim_funcname
+        timer_id = int(vim.eval(f"timer_start({interval}, '{vim_funcname}', {{'repeat': 1}})"))
+        return timer_id
+
+
+if  __name__ == "__main__":
+    def hello():
+        print("H")
+    TimerTask.register(hello)
