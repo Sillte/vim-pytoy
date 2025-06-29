dbb414f0735a9ba7ee16251fc2025fabac974a93
sillte <innocent.konoha@gmail.com>
2025-06-29 23:59:50 +0900

    Refactor

--- pythonx/pytoy/tools/__init__.py
+++ pythonx/pytoy/tools/__init__.py
@@ -1,4 +1,4 @@
-from pytoy.tools.python_executor import PythonExecutor
+from pytoy.tools.python.python_executor import PythonExecutor
 from pytoy.tools.pytest import PytestExecutor
-from pytoy.tools.pytools_executors import MypyExecutor
+from pytoy.tools.mypy import MypyExecutor
 from pytoy.tools.pytools_executors import RuffExecutor
--- pythonx/pytoy/tools/mypy/__init__.py
+++ pythonx/pytoy/tools/mypy/__init__.py
@@ -4,21 +4,8 @@ import re
 from pytoy.lib_tools.buffer_executor import BufferExecutor
 
 from pytoy.lib_tools.environment_manager import EnvironmentManager
-from pytoy.ui import PytoyBuffer, PytoyQuickFix
+from pytoy.ui import PytoyQuickFix
 
-# def _set_focus(win_id: int, topline: int | None = None):
-#    height = int(vim.eval(f"winheight({win_id})"))
-#    if topline is None:
-#        import json
-#        info = json.loads(vim.eval(f"json_encode(getwininfo({win_id}))"))
-#        topline = int(info[0]["topline"])
-#    if topline <= 0:
-#        topline = 1
-#    centerline = topline + height // 2
-#    vim.command(f"call win_execute({win_id}, 'call cursor({centerline},1)')")
-#
-# This kind of function should be implemented `PytoyBuffer`.
-#
 
 
 def _to_quickfix_winid(win_id: int, is_location: bool | None = None) -> int | None:
@@ -85,46 +72,5 @@ class MypyExecutor(BufferExecutor):
                 continue
             record = match.groupdict()
             record["type"] = record["_type"].strip(" ")[0].upper()
-            records.append(record)
-        return records
-
-
-class RuffExecutor(BufferExecutor):
-    """Execute Ruff."""
-
-    def __init__(self):
-        self._pattern = re.compile(
-            r"(?P<filename>.+):(?P<lnum>\d+):(?P<col>\d+):(?P<text>(.+))"
-        )
-
-    def check(self, args: str | list, stdout, command_wrapper=None):
-        if command_wrapper is None:
-            command_wrapper = EnvironmentManager().get_command_wrapper()
-
-        if isinstance(args, list):
-            args = " ".join(args)
-        command = f"ruff check {args}"
-        return super().run(command, stdout, stdout, command_wrapper=command_wrapper)
-
-    def prepare(self):
-        # I do not need to return any `dict` for customization.
-        # But, it requires `win_id` at `on_closed`.
-        self.win_id = vim.eval("win_getid()")
-        return None
-
-    def on_closed(self):
-        assert self.stdout is not None
-        messages = self.stdout.content
-        qflist = self._make_qflist(messages)
-        q_winid = _to_quickfix_winid(self.win_id)
-        _handle_loclist(q_winid, qflist, is_open=True)
-
-    def _make_qflist(self, string):
-        records = []
-        for line in string.split("\n"):
-            match = self._pattern.match(line.strip())
-            if not match:
-                continue
-            record = match.groupdict()
             records.append(record)
         return records
