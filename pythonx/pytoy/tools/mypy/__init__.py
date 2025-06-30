506dd98028f4aa942004c836192e3a34bfb59daf
sillte <innocent.konoha@gmail.com>
2025-07-01 01:04:55 +0900

    revise

--- pythonx/pytoy/tools/mypy/__init__.py
+++ pythonx/pytoy/tools/mypy/__init__.py
@@ -7,24 +7,13 @@ from pytoy.lib_tools.environment_manager import EnvironmentManager
 from pytoy.ui import PytoyQuickFix
 
 
-
-def _to_quickfix_winid(win_id: int, is_location: bool | None = None) -> int | None:
-    if is_location is None:
-        is_location = False  # Default behavior.
-
-    if is_location:
-        return win_id
-    else:
-        return None
-
-
-def _handle_loclist(win_id, records: list[dict], is_open: bool):
+def _handle_records(records: list[dict], is_open: bool):
     if records:
-        PytoyQuickFix().setlist(records, win_id)
+        PytoyQuickFix().setlist(records, win_id=None)
         if is_open:
-            PytoyQuickFix().open(win_id)
+            PytoyQuickFix().open(win_id=None)
     else:
-        PytoyQuickFix().close(win_id)
+        PytoyQuickFix().close(win_id=None)
 
 
 class MypyExecutor(BufferExecutor):
@@ -43,25 +32,14 @@ class MypyExecutor(BufferExecutor):
         # stdout[0] = command
         return super().run(command, stdout, stdout, command_wrapper=command_wrapper)
 
-    def prepare(self):
-        # I do not need to return any `dict` for customization.
-        # But, it requires `win_id` at `on_closed`.
-        self.win_id = vim.eval("win_getid()")
-        return None
 
     def on_closed(self):
         assert self.stdout is not None
         messages = self.stdout.content
 
         qflist = self._make_qflist(messages)
-        q_winid = _to_quickfix_winid(self.win_id)
-        _handle_loclist(q_winid, qflist, is_open=True)
+        _handle_records(qflist, is_open=True)
 
-        # Scrolling output window
-        # [NOTE ](2025/06/22): I feel this function is not necessary.
-        # with store_cursor():
-        #    self.stdout.focus()
-        #    vim.command("normal zb")
 
     def _make_qflist(self, string):
         # Record of `mypy`.
