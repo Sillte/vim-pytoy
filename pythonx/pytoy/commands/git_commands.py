5dff0cf20f53b7dd2acd1bba027892fdbc7e8c77
sillte <innocent.konoha@gmail.com>
2025-08-05 23:01:56 +0900

    [fix] add git command

--- pythonx/pytoy/commands/git_commands.py
+++ pythonx/pytoy/commands/git_commands.py
@@ -2,15 +2,16 @@
 """
 import vim 
 from pathlib import Path
-from pytoy.command import CommandManager
+from pytoy.command import CommandManager, OptsArgument
 from pytoy.tools.git import get_remote_link
 
 @CommandManager.register(name="GitAddress", range="")
-def get_gitaddress(opts: dict):
-    line1 = opts["line1"] 
+def get_gitaddress(opts: OptsArgument):
+    line1 = opts.line1
+    line2 = opts.line2
+    
     filepath = Path(vim.current.buffer.name)
-    vim.current.buffer.name
-    remote_link = get_remote_link(filepath, line1)
+    remote_link = get_remote_link(filepath, line1, line2)
     vim.command(f'let @c="{remote_link}"')
     vim.command(f'let @*="{remote_link}"')
     print("@c/@*", remote_link)
