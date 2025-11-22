da77755874b8cd50a531f0293f0ef483f8b3628c
sillte <innocent.konoha@gmail.com>
2025-11-22 15:21:32 +0900

    Vim env fixed and Refactor

--- pythonx/pytoy/lib_tools/buffer_executor/buffer_job_manager.py
+++ pythonx/pytoy/lib_tools/buffer_executor/buffer_job_manager.py
@@ -9,10 +9,10 @@ from pytoy.lib_tools.buffer_executor.buffer_job import (
 )
 
 
-from dataclasses import dataclass 
+from dataclasses import dataclass
 from typing import Mapping
 
-        
+
 @dataclass
 class BufferJobCreationParam:
     command: str | list[str]
@@ -21,18 +21,19 @@ class BufferJobCreationParam:
     cwd: Path | str | None = None
     env: Mapping[str, str] | None = None
     on_closed: Callable[[BufferJobProtocol], Any] | None = None
-        
+
 
 type BufferJobName = str
-class BufferJobManager:
 
+
+class BufferJobManager:
     buffer_jobs: dict[str, BufferJobProtocol] = dict()
-    
+
     @staticmethod
     def create(name: str, creation_param: BufferJobCreationParam) -> BufferJobProtocol:
         if BufferJobManager.is_running(name):
-                msg = f"`{name}` buffer job is already running."
-                raise ValueError(msg)
+            msg = f"`{name}` buffer job is already running."
+            raise ValueError(msg)
 
         buffer_job = make_buffer_job(
             name=name,
@@ -40,25 +41,27 @@ class BufferJobManager:
             stderr=creation_param.stderr,
         )
 
-        stdout=creation_param.stdout
-        stderr=creation_param.stderr
+        stdout = creation_param.stdout
+        stderr = creation_param.stderr
         command = creation_param.command
-        env=creation_param.env
-        cwd=creation_param.cwd
+        env = creation_param.env
+        cwd = creation_param.cwd
         if not isinstance(command, str):
-            command = " ".join(command) 
+            command = " ".join(command)
 
         if stdout:
             stdout.init_buffer(command)
         if stderr:
             stderr.init_buffer()
 
-        BufferJobManager.buffer_jobs[name] = buffer_job 
-        
+        BufferJobManager.buffer_jobs[name] = buffer_job
+
         def prepare() -> Mapping:
             return {}
+
         def dummy_on_closed(buffer_job: BufferJobProtocol) -> None:
             return None
+
         on_closed = creation_param.on_closed or dummy_on_closed
         buffer_job.job_start(command, prepare, on_closed, cwd=cwd, env=env)
         return buffer_job
@@ -70,16 +73,14 @@ class BufferJobManager:
     @staticmethod
     def is_running(name: str) -> bool:
         buffer_job = BufferJobManager.get(name)
-        print("buffer_job_is_running", buffer_job)
         return bool(buffer_job and buffer_job.is_running)
-    
+
     @staticmethod
     def stop(name: str) -> None:
         buffer_job = BufferJobManager.get(name)
         if buffer_job:
-            buffer_job.stop() 
+            buffer_job.stop()
 
-    
 
 if __name__ == "__main__":
     pass
--- pythonx/pytoy/lib_tools/buffer_executor/command_wrapper.py
+++ pythonx/pytoy/lib_tools/buffer_executor/command_wrapper.py
@@ -1,5 +1,5 @@
 from typing import Callable
+
 type CommandWrapper = Callable[[str], str]
 
 naive_command_wrapper = lambda cmd: cmd
-
