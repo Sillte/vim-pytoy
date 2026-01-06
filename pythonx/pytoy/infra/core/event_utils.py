ecbab0681dfe0b8b0e14d54747cc87284927b81d
sillte <innocent.konoha@gmail.com>
2026-01-07 01:25:33 +0900

    Revise

---
+++ pythonx/pytoy/infra/core/event_utils.py
@@ -0,0 +1,41 @@
+from pytoy.infra.core.models import Disposable, Event
+from pytoy.infra.core.models import  Listener
+from typing import Callable
+
+
+def once[T](event: Event[T]) -> Event[T]:
+    def subscribe(listener: Listener[T]) -> Disposable:
+        alive_disposable: Disposable | None  = None
+        def wrapper(value: T) -> None:
+            nonlocal alive_disposable
+            if alive_disposable is None: 
+                return 
+            listener(value)
+            alive_disposable.dispose()
+            alive_disposable = None
+
+        alive_disposable = event.subscribe(wrapper)
+        return Disposable(lambda : alive_disposable.dispose()
+                          if alive_disposable is not None else None) 
+    return Event(subscribe)
+
+
+def map_event[T, U](event: Event[T], transform: Callable[[T], U]) -> Event[U]:
+    """If the source of event is disposed, then the returned Event is not fired. 
+    """
+    def subscribe(listener: Listener[U]) -> Disposable:
+        return event.subscribe(lambda value: listener(transform(value)))
+    return Event[U](subscribe)
+
+def filter[T](event: Event[T], predicator: Callable[[T], bool]) -> Event[T]:
+    def subscribe(listener: Listener[T]) -> Disposable:
+        def wrapper(value: T) -> None:
+            if predicator(value):
+                listener(value)
+            else:
+                ...
+        disposable = event.subscribe(wrapper)
+        return Disposable(lambda : disposable.dispose()) 
+    return Event(subscribe)
+
+
