fa272ac90857f8a741516aa583ad98a9d295dda6
sillte <innocent.konoha@gmail.com>
2026-04-04 03:02:36 +0900

    Add `Completer`..

--- pythonx/pytoy/command.py
+++
@@ -1,6 +0,0 @@
-# This module is a alias of `pytoy.shared.command`.
-# The below three classes are the public interfaces
-# intended to be used.
-
-from pytoy.shared.old_command import CommandManager  # NOQA
-from pytoy.shared.old_command.models import OptsArgument  # NOQA
