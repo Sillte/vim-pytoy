c4509b2700a21a32c3d6272957d070b71ba9b4d4
sillte <innocent.konoha@gmail.com>
2026-08-16 15:07:25 +0900

    Refactor

--- pythonx/pytoy/commands/experiment_commands.py
+++ pythonx/pytoy/commands/experiment_commands.py
@@ -9,7 +9,7 @@ app = App()
 @app.command("GatherTextFiles")
 def gather_text_files():
     from pytoy_llm.materials.text_files import TextFilesCollector
-    from pytoy_llm.materials.composers import NaiveSectionComposer
+    from pytoy_llm.materials.composers import MaterialDataExplorerTaskComposer
     from pytoy.job_execution.environment_manager import EnvironmentManager
 
     buffer = PytoyBuffer.get_current()
@@ -20,8 +20,8 @@ def gather_text_files():
     workspace = workspace or path.parent
     collector = TextFilesCollector(path, workspace=workspace)
     bundle = collector.bundle
-    composer = NaiveSectionComposer([bundle.text_section_data])
-    section_text = composer.compose_prompt()
+    composer = MaterialDataExplorerTaskComposer([bundle.text_section_data])
+    section_text = composer.compose_system_prompt()
     buffer = make_buffer("__docs__", "vertical")
     buffer.init_buffer()
     buffer.append(section_text)
@@ -31,7 +31,7 @@ def gather_text_files():
 def gather_git_diffs():
     from pytoy_llm.materials.git_diffs.collectors import GitDiffCollector
     from pytoy_llm.materials.git_diffs.models import GitDiffBundleQuery
-    from pytoy_llm.materials.composers import NaiveSectionComposer
+    from pytoy_llm.materials.composers import MaterialDataExplorerTaskComposer
     from pytoy.job_execution.environment_manager import EnvironmentManager
     from pathlib import Path
 
@@ -45,8 +45,8 @@ def gather_git_diffs():
     collector = GitDiffCollector(path)
     query = GitDiffBundleQuery()
     bundle = collector.get_bundle(query)
-    composer = NaiveSectionComposer([bundle.model_section_data])
-    section_text = composer.compose_prompt()
+    composer = MaterialDataExplorerTaskComposer([bundle.text_section_data])
+    section_text = composer.compose_system_prompt()
     buffer = make_buffer("__docs__", "vertical")
     buffer.init_buffer()
     buffer.append(section_text)
